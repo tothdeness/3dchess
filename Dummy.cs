a81fc9fcd513644333348372515feef75092a0e9
tothdeness <szarumanganden@gmail.com>
2024-03-04 22:07:05 +0100

    Add files

---
+++ Dummy.cs
@@ -0,0 +1,99 @@
+using Godot;
+using System;
+
+public partial class Dummy : MeshInstance3D
+{
+
+
+	private bool figureLocked = false;
+	private MeshInstance3D piece;
+	private StandardMaterial3D standardMaterial = new StandardMaterial3D();
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("LOADED" + Name);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void _on_static_body_3d_mouse_entered()
+	{
+		//GD.Print("You have entered the ccube!!!");
+	}
+
+
+
+
+	public override void _Input(InputEvent _event)
+	{
+		if(_event is InputEventMouseButton && figureLocked)
+		{
+			InputEventMouseButton mouse = (InputEventMouseButton)_event;
+
+			if(mouse.ButtonIndex == MouseButton.Left)
+			{
+				figureLocked = false;
+
+
+				piece = GetNode<MeshInstance3D>("../"+Name);
+
+				standardMaterial.AlbedoColor = new Color(1.0f, 1.0f, 1.0f);
+				piece.MaterialOverlay = standardMaterial;
+
+
+				GD.Print("Figura unlocked!");
+				
+			
+
+				//QueueFree();
+			}
+
+		}
+
+	}
+
+
+
+
+	private void _on_static_body_3d_input_event(Node camera, InputEvent _event, Vector3 position, Vector3 normal, long shape_idx)
+	{
+		
+
+		if(_event is InputEventMouseButton)
+		{
+
+			InputEventMouseButton mouse = (InputEventMouseButton)_event;
+
+
+			if ( mouse.ButtonIndex == MouseButton.Left && !mouse.IsReleased() )
+			{
+				piece = GetNode<MeshInstance3D>("../"+Name);
+
+
+
+				standardMaterial.AlbedoColor = new Color(0.0f, 1.0f, 0.0f);
+				piece.MaterialOverlay = standardMaterial;
+
+				figureLocked = true;
+
+				GD.Print("Figura locked!");
+			}
+			
+
+		}
+
+
+
+
+	}
+
+
+}
+
+
+
---
+++ main.cs
@@ -0,0 +1,106 @@
+using Godot;
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+public partial class main : Node3D
+{
+
+	private Vector2 draggingPosition;
+	private Vector2 currentPos;
+	private bool rightMouseButtonIsPressed = false;
+	private Node3D cameraHelper;
+
+	public override void _Ready()
+	{
+		GD.Print("Udvozlet a 3D sakk jatekban!");
+
+		var scene = GD.Load<PackedScene>("res://mesh.tscn");
+		var inst = scene.Instantiate();
+		AddChild(inst);
+
+
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+
+	public override void _Input(InputEvent _event)
+	{
+
+	
+		base._Input(_event);
+
+		if (_event is InputEventMouseButton)
+		{
+			InputEventMouseButton mouse = (InputEventMouseButton)_event;
+
+
+			if (mouse.ButtonIndex == MouseButton.Right)
+			{
+				draggingPosition = mouse.Position;
+				rightMouseButtonIsPressed = !rightMouseButtonIsPressed;
+
+				if (rightMouseButtonIsPressed)
+				{
+					GD.Print("Eger locked! " + draggingPosition);
+				}
+				else
+				{
+					GD.Print("Eger unlocked!");
+				}
+
+
+			}
+
+
+		} else if (_event is InputEventMouseMotion && rightMouseButtonIsPressed) {
+
+			InputEventMouseMotion mouse = (InputEventMouseMotion)_event;
+
+
+			cameraHelper = GetNode<Node3D>("Node3D");
+
+			//GD.Print("Eger mozgatas! " + mouse.Position);
+
+
+			currentPos = mouse.Position;
+
+
+			Vector2 distance = draggingPosition - currentPos;
+
+
+
+			if (Math.Abs(distance.X) > 2)
+			{
+				cameraHelper.RotateY(0.02f * (distance.X > 0 ? 1 : -1));
+				//cameraHelper.RotateObjectLocal(new Vector3(0, 1, 0), 0.02f * (distance.X > 0 ? 1 : -1));
+				draggingPosition = currentPos;
+			}
+
+
+
+
+			if (Math.Abs(distance.Y) > 2)
+			{
+
+
+
+				cameraHelper.RotateObjectLocal(new Vector3(1, 0, 0), 0.02f * (distance.Y > 0 ? 1 : -1));
+				//cameraHelper.RotateX(0.02f * (distance.Y > 0 ? 1 : -1));
+				draggingPosition = currentPos;
+			}
+
+		
+
+
+
+		}
+
+
+	}
+
+}
+
+
