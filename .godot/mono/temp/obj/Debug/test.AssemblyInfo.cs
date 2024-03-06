617c9113a57eda6e460669c671e5067f89918f3a
tothdeness <szarumanganden@gmail.com>
2024-03-07 00:47:26 +0100

    add- chess table

--- .godot/mono/temp/obj/Debug/test.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eab8110bf4dc21f44cf4bb893ce470df68e26cd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec38da3f7ce6c9e892625b913b63106f8c467588")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
