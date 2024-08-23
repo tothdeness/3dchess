e7131eb5c8030416aff34dd2f807d42c1fd80cf5
tothdeness <szarumanganden@gmail.com>
2024-08-23 03:52:29 +0200

    deleted unnecessary files

--- .godot/mono/temp/obj/ExportRelease/win-x64/test.AssemblyInfo.cs
+++ .godot/mono/temp/obj/ExportRelease/win-x64/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportRelease")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12bc5934a0c94b0944efd7a06deb7223f9813ae3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ba83217cbaddfbb6cdf44d69ba79025f499305")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
