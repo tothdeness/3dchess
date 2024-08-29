3c5b3033b296651306eee6da1f8822fe5cd9b6da
tothdeness <szarumanganden@gmail.com>
2024-08-29 21:20:32 +0200

    add: background texture

--- .godot/mono/temp/obj/Debug/test.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7131eb5c8030416aff34dd2f807d42c1fd80cf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+094a4315da3c3c673fc9d8b335ba4821c30b64b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
